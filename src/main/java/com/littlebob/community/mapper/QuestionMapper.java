3ecb20baaa0bc92587958b0d5e06ee4514713e83
FightingBob <869674151@qq.com>
2019-09-12 15:25:03 +0800

    add question table, mapper

---
+++ src/main/java/com/littlebob/community/mapper/QuestionMapper.java
@@ -0,0 +1,12 @@
+package com.littlebob.community.mapper;
+
+import com.littlebob.community.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface QuestionMapper {
+
+    @Insert("insert into question (title, description, creator, gmt_create, gmt_modified, view_count, comment_count, like_count, tag) values(#{title}, #{description}, #{creator}, #{gmtCreate}, #{gmtModified}, #{viewCount}, #{commentCount}, #{like_count}, #{tag})")
+    void insert(Question question);
+}
