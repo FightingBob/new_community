da11d5054ae8ea0e2052e6bd39eb7507dcdcd90c
FightingBob <869674151@qq.com>
2019-09-12 09:19:14 +0800

    实现持久化登录状态

--- src/main/java/com/littlebob/community/mapper/UserMapper.java
+++ src/main/java/com/littlebob/community/mapper/UserMapper.java
@@ -3,10 +3,18 @@ package com.littlebob.community.mapper;
 import com.littlebob.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
 
     @Insert("insert into user (name, count_id, token, gmt_create, gmt_modified) values(#{name}, #{countId}, #{token}, #{gmtCreate}, #{gmtModified})")
     void insert(User user);
+
+    @Select("select id from user where count_id = #{countId}")
+    int select(String countId);
+
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
